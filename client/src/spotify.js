a7e797f24a279aadfb142ee8506cd77957be7b69
Daniil <onejump9@gmail.com>
2021-10-18 12:56:38 +0300

    Fetching and displaying data from spotify

--- client/src/spotify.js
+++ client/src/spotify.js
@@ -1,4 +1,5 @@
 import axios from "axios";
+import { header } from "express/lib/request";
 
 // Map for localStorage keys
 const LOCALSTORAGE_KEYS = {
@@ -128,3 +129,18 @@ const getAccessToken = () => {
 };
 
 export const accessToken = getAccessToken();
+
+/**
+ * Axios global request headers
+ * https://github.com/axios/axios#global-axios-defaults
+ */
+axios.defaults.baseURL = "https://api.spotify.com/v1";
+axios.defaults.headers["Authorization"] = `Bearer ${accessToken}`;
+axios.defaults.headers["Content-Type"] = "application/json";
+
+/**
+ * Get Current User's Profile
+ * https://developer.spotify.com/documentation/web-api/reference/#endpoint-get-current-users-profile
+ * @returns {Promise}
+ */
+export const getCurrentUserProfile = () => axios.get("/me");
