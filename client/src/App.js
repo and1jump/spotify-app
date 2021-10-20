24ac0b1ff162bd3d0371a949d55edfb0ae9e7348
Daniil <onejump9@gmail.com>
2021-10-20 14:25:55 +0300

    adding top tracks and artists pages

--- client/src/App.js
+++ client/src/App.js
@@ -7,7 +7,7 @@ import {
 } from "react-router-dom";
 import { accessToken, logout } from "./spotify";
 import { GlobalStyle } from "./styles";
-import { Login, Profile } from "./pages";
+import { Login, Profile, TopArtists, TopTracks } from "./pages";
 import styled from "styled-components/macro";
 
 const StyledLogoutButton = styled.button`
@@ -58,10 +58,10 @@ function App() {
 
               <Switch>
                 <Route path="/top-artists">
-                  <h1>Top Artists</h1>
+                  <TopArtists />
                 </Route>
                 <Route path="/top-tracks">
-                  <h1>Top Tracks</h1>
+                  <TopTracks />
                 </Route>
                 <Route path="/playlists/:id">
                   <h1>Playlist</h1>
